54029aedb5970640cf630806e693bc9e6322f913
William Villanueva <wvillanu89@gmail.com>
2014-05-15 10:31:02 -0700

    set up original problem, and had it pass tests that only contain arrays, strings, booleans, null values, and numbers.  Next step is to make stringifyJSON compatible with objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,39 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringJSON = [];
+  function stringFill(objorlist,indexorkey,tracker) {
+  	if (Array.isArray(objorlist)) {
+  		stringJSON.push('[');
+  		_.each(objorlist,function(value,index,objorlist){stringFill(value,index,objorlist);})
+  		stringJSON.push(']');
+  		if(indexorkey!==undefined && indexorkey<tracker.length-1){
+  			stringJSON.push(',');
+  		}
+    }else if (typeof(objorlist)==='string'){
+  		stringJSON.push('\"');
+  		stringJSON.push(objorlist)
+  		stringJSON.push('\"');
+        if(indexorkey!==undefined && indexorkey<tracker.length-1){
+  			stringJSON.push(',');
+  		}
+    }else if (typeof(objorlist)==='number'){
+    	stringJSON.push(objorlist)
+    	if(indexorkey!==undefined && indexorkey<tracker.length-1){
+  			stringJSON.push(',');
+  		}
+  	}else if (objorlist===null) {
+  		stringJSON.push('null')
+    	if(indexorkey!==undefined && indexorkey<tracker.length-1){
+  			stringJSON.push(',');
+  		}
+  	}else if (typeof(objorlist)==='boolean'){
+		objorlist ? stringJSON.push('true') : stringJSON.push('false');
+    	if(indexorkey!==undefined && indexorkey<tracker.length-1){
+  			stringJSON.push(',');
+  		}
+  	}
+  };
+  stringFill(obj);
+  return stringJSON.join('');
 };
