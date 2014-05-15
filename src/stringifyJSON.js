a6cd1bd48cd0c4c202a2c6436135a0846450aca7
William Villanueva <wvillanu89@gmail.com>
2014-05-15 11:42:13 -0700

    Improved code conciseness.  placed repetetitve if condition that adds comma at the end stringFill function instead of each conditional statement.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -10,31 +10,16 @@ var stringifyJSON = function(obj) {
   		stringJSON.push('[');
   		_.each(objorlist,function(value,index,objorlist){stringFill(value,index,objorlist);})
   		stringJSON.push(']');
-  		if(indexorkey!==undefined && indexorkey<tracker.length-1){
-  			stringJSON.push(',');
-  		}
     }else if (typeof(objorlist)==='string'){
   		stringJSON.push('\"');
   		stringJSON.push(objorlist)
   		stringJSON.push('\"');
-        if(indexorkey!==undefined && indexorkey<tracker.length-1){
-  			stringJSON.push(',');
-  		}
     }else if (typeof(objorlist)==='number'){
-    	stringJSON.push(objorlist)
-    	if(indexorkey!==undefined && indexorkey<tracker.length-1){
-  			stringJSON.push(',');
-  		}
+    	stringJSON.push(objorlist);
   	}else if (objorlist===null) {
-  		stringJSON.push('null')
-    	if(indexorkey!==undefined && indexorkey<tracker.length-1){
-  			stringJSON.push(',');
-  		}
+  		stringJSON.push('null');
   	}else if (typeof(objorlist)==='boolean'){
 		objorlist ? stringJSON.push('true') : stringJSON.push('false');
-    	if(indexorkey!==undefined && indexorkey<tracker.length-1){
-  			stringJSON.push(',');
-  		}
   	}else if(typeof(objorlist)==='object'){
   		var objLength = _.keys(objorlist).length;
   		console.log(objLength);
@@ -54,11 +39,10 @@ var stringifyJSON = function(obj) {
   			i++
   		});
   		stringJSON.push('}');
-  		if(indexorkey!==undefined && indexorkey<tracker.length-1){
-  			stringJSON.push(',');
   		}
+  	if(indexorkey!==undefined && indexorkey<tracker.length-1){
+  			stringJSON.push(',');
   	}
-
   };
   stringFill(obj);
   return stringJSON.join('');
